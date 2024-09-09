f25411e186373e1dc3a128118620072337b5b8ee
Stephen Slatky <steve.slatky@gmail.com>
2024-09-09 16:04:48 -0400

    change to main

--- testy.go
+++ testy.go
@@ -1,24 +1,31 @@
-package testy
+package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 const Version = "0.0.1"
 
 type test struct {
-    name string 
-    vars []Fv 
+	name string
+	vars []Fv
 }
 
 // Formatd var
 type Fv struct {
-    name string 
-    T    any
+	name string
+	T    any
 }
 
 // AssertEqual checks if two values are equal
 func AssEq(t *testing.T, expected, actual interface{}) {
-    t.Helper()
-    if expected != actual {
-        t.Errorf("Expected result: %d, Actual Result %d", expected, actual)
-    }
+	t.Helper()
+	if expected != actual {
+		t.Errorf("Expected result: %d, Actual Result %d", expected, actual)
+	}
+}
+
+func main(){
+    fmt.Println("Worked")
 }
